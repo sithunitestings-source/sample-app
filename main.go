ad0370c9e85db4ebf79469ef0c115ada04a80097
sithunitestings-source <student-03-bccd3351f6e6@qwiklabs.net>
2026-05-31 15:54:21 +0000

    Dr Abhishek

--- main.go
+++ main.go
@@ -26,4 +26,5 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
+	http.HandleFunc("/red", redHandler)
 }
